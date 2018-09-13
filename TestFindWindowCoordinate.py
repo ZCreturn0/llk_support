253145c729c463bb20332f72e33c016ce1a8404e
zcreturn0 <237350543@qq.com>
2018-09-13 11:20:22 +0800

    combine mouseMove and mouseClick

--- TestFindWindowCoordinate.py
+++ TestFindWindowCoordinate.py
@@ -1,4 +1,6 @@
-import win32gui 
+from ctypes import *
+import win32api,win32gui,win32con
+import time
 hwnd = win32gui.FindWindow(None, '计算器')
 rect = win32gui.GetWindowRect(hwnd)
 x = rect[0]
@@ -8,3 +10,6 @@ h = rect[3] - y
 print("Window %s:" % (win32gui.GetWindowText(hwnd)))
 print("Location: (%d, %d)" % (x, y))
 print("Size: (%d, %d)" % (w, h))
+time.sleep(1)
+windll.user32.SetCursorPos((x+w-10), (y+10))
+win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN|win32con.MOUSEEVENTF_LEFTUP, 0, 0)
